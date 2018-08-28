bcca9338cacb9c62303e577ac55c5af356d9db8b
longhoang94 <longsutls@gmail.com>
2018-08-28 10:35:22 +0700

    pass all test-case

--- src/main/java/fanboy/IAmFan.java
+++ src/main/java/fanboy/IAmFan.java
@@ -1,9 +1,10 @@
 package fanboy;
 
 public class IAmFan {
-    private final int SLOW = 1;
-    private final int MEDIUM = 2;
-    private final int FAST = 3;
+    static final int SLOW = 1;
+    static final int MEDIUM = 2;
+    static final int FAST = 3;
+
     private int speed = SLOW;
     private boolean on = false;
     private double radius = 5;
@@ -17,15 +18,15 @@ public class IAmFan {
     }
 
     public int setSpeed(int speed) {
-        return this.speed;
+        return this.speed = speed;
     }
 
     public boolean isOn() {
         return on;
     }
 
-    public boolean setOn() {
-        return this.on;
+    public boolean setOn(boolean on) {
+        return this.on = on;
     }
 
     public double getRadius() {
@@ -46,7 +47,7 @@ public class IAmFan {
 
     public String toString() {
         if (isOn()) {
-            return "Speed: " + getSpeed() + "\nColor: "+getColor() + "\nRadius: "+ getRadius() + "\nfan is on";
-        } else return "Speed: " + getSpeed() + "\nColor: "+getColor() + "\nRadius: "+ getRadius() + "\nfan is off";
+            return "Speed: " + getSpeed() + "\nColor: " + getColor() + "\nRadius: " + getRadius() + "\nfan is on";
+        } else return "Speed: " + getSpeed() + "\nColor: " + getColor() + "\nRadius: " + getRadius() + "\nfan is off";
     }
 }
